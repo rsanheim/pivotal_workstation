d9499c683db26e0bae494811267d8497b53f1728
Jess Martin <jessmartin@gmail.com>
2011-11-18 18:19:30 -0500

    Disabling failing recipes

--- recipes/meta_ruby_development.rb
+++ recipes/meta_ruby_development.rb
@@ -1,6 +1,6 @@
 include_recipe "pivotal_workstation::rvm"
 include_recipe "pivotal_workstation::bash_profile-no_sudo_gem_install"
 include_recipe "pivotal_workstation::mysql"
-include_recipe "pivotal_workstation::postgres"
+# include_recipe "pivotal_workstation::postgres"
 include_recipe "pivotal_workstation::imagemagick"
 include_recipe "pivotal_workstation::gem_no_rdoc_no_ri"
